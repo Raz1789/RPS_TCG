95a950784e8f000f7667e8a8520c9dbbf6ff615b
Raz1789 <navaneeth_3@hotmail.com>
2017-10-24 23:39:48 +0400

    Implemented the Camera Movement

--- Assets/Resources/Scripts/CameraMovement.cs
+++ Assets/Resources/Scripts/CameraMovement.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+    Vector2 cameraPosition;
+
 	// Use this for initialization
 	void Start () {
-		
+        cameraPosition = new Vector2();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if(Input.touchCount > 0)
         {
-
+            cameraPosition = transform.position;
+            if(Input.GetTouch(0).deltaPosition.y != 0)
+            {
+                float newCameraY = cameraPosition.y - Input.GetTouch(0).deltaPosition.y * Time.deltaTime;
+                if(newCameraY <= 6.27 && newCameraY >= -6.27)
+                {
+                    transform.position = new Vector3(transform.position.x, newCameraY, transform.position.z);
+                }
+            }
         }
 	}
 }
